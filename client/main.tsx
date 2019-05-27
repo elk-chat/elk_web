a25a7b0417c7c077b2d1dcf4cacce5184929c781
ALEX <zhangxiangjie@zhangxiangjies-MacBook.local>
2019-05-27 14:24:14 +0800

    add telint

--- client/main.tsx
+++ client/main.tsx
@@ -11,7 +11,7 @@ class App extends React.Component<AppProps> {
     color: 'red'
   };
   render() {
-    console.log(this.props)
+    console.log(this.props);
     return (
       <div
         style={{

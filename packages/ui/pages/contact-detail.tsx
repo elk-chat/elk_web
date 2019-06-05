040b867143e8b44528ca67c7d42fb7181881c2af
SANGET <3348592+SANGET@users.noreply.github.com>
2019-06-05 22:54:59 +0800

    新增 ContactDetail

---
+++ packages/ui/pages/contact-detail.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { UserInfo } from '@little-chat/core/types';
+
+interface ContactEntity {
+  UserName: string;
+  Avatar: string;
+}
+
+interface ContactDetailProps extends UserInfo {
+  contact: ContactEntity;
+}
+
+export default class ContactDetail extends React.Component<ContactDetailProps, {}> {
+  render() {
+    const { contact } = this.props;
+    const { UserName, Avatar } = contact;
+
+    return (
+      <div className="contact-detail">
+        {UserName}
+      </div>
+    );
+  }
+}

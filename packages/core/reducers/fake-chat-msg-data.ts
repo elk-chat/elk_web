497a547ab5c31d2ce95e88d0086e7534788bff0b
SANGET <3348592+SANGET@users.noreply.github.com>
2019-06-16 13:14:21 +0800

    add poetry

--- packages/core/reducers/fake-chat-msg-data.ts
+++ packages/core/reducers/fake-chat-msg-data.ts
@@ -3,7 +3,9 @@ const FakeMsgDataGroup = [
   '这大都会的黄昏很迷人，可我偶尔会有挥之不去的孤寂，每当看见那些囊中羞涩的年轻职员在商店橱窗前徜徉，挨到晚饭时间形影相吊地去餐厅填肚子，我知道他们也深有同感——我们这些薄暮中的年轻职员啊，正在虚度一生中最灿烂的年华，一夜中最美好的时辰。',
   '人生就像一片涮羊肉：浮浮沉沉，备受煎熬，尝尽各种滋味，一不留神就老了，不变的是那一阵淡淡的骚味儿',
   '人事有代谢，往来成古今。江山留胜迹，我辈复登临。水落鱼梁浅，天寒梦泽深。羊公碑尚在，读罢泪沾襟。',
-  'In winter I get up at night, And dress by yellow candle-light. In summer quite the other way, I have to go to bed by day.'
+  '渐渐地，我终于发现命运是我怯懦时的盾牌：当我叫嚷命运不公最响的时候，正是我预备逃遁的前奏。命运像一只筐，我把自己对自己的姑息、原谅以及所有的延宕都一股脑儿地塞进去，然后蒙一块宿命的轻纱。我背着它慢慢地向前走，心中有一份心安理得的坦然。而有意思的是，当我们迷茫、懒惰、推送的时候，我们会格外相信命运；而当我们努力拼搏，积极向上时，我们会格外相信自己。',
+  'In winter I get up at night, And dress by yellow candle-light. In summer quite the other way, I have to go to bed by day.',
+  'Three passions, simple but overwhelmingly strong, have governed my life: the longing for love, the search for knowledge, and unbearable pity for the suffering of mankind. These passions, like great winds, have blown me hither and thither, in a wayward course over a deep ocean of anguish, reaching to the very verge of despair.',
 ];
 
 export default FakeMsgDataGroup;

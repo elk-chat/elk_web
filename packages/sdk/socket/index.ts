48a2c5e29d65928b6835932cd7dbd0e890fdd696
SANGET <AlexZhang@Alexs-iMac.local>
2019-06-18 16:55:24 +0800

    封装 websocket 辅助类

--- packages/sdk/socket/index.ts
+++ packages/sdk/socket/index.ts
@@ -1,20 +1,89 @@
+import { EventEmitterClass } from 'basic-helper';
+
 interface SocketParams {
-  url: string;
-  onOpen: () => void;
-  onMessage: (event: MessageEvent) => void;
+  /** 链接的 apiHost */
+  apiHost: string;
+  /** ws 发送请求前的 hook */
+  beforeWSHook?: (any) => any;
+  /** ws onmessage 时的 hook */
+  afterWSHook?: (any) => any;
+  /** 打开 socket 链接 */
+  onOpen?: () => void;
+  /** 失败 */
+  onErr?: () => void;
+  onMessage?: (event: MessageEvent) => void;
+}
+const onOpenMark = 'onOpen';
+const onMessageMark = 'onMessage';
+
+function wrapWSUrl(hostname) {
+  return `ws://${hostname}`;
 }
 
-export function connectSocket(params: SocketParams): WebSocket {
-  const { url, onOpen, onMessage } = params;
-  const socket = new WebSocket(url);
+class SocketHelper extends EventEmitterClass {
+  socket!: WebSocket;
 
-  socket.onopen = () => {
-    onOpen();
-  };
-  socket.onmessage = (event) => {
-    onMessage(event);
-    // socket.send(encodeBuffer);
-  };
+  params!: SocketParams;
 
-  return socket;
+  connecting: boolean = false;
+
+  constructor(params: SocketParams) {
+    super();
+    const { apiHost } = params;
+    this.params = params;
+    if (apiHost) {
+      this.initWS(apiHost);
+    } else {
+      console.error('请传入 apiHost');
+    }
+  }
+
+  initWS = (apiHost) => {
+    const wsApiHost = wrapWSUrl(apiHost);
+    this.socket = new WebSocket(wsApiHost);
+    // this.socket.binaryType = 'arraybuffer';
+
+    this.socket.onopen = this.onOpen;
+    this.socket.onmessage = this.onMessage;
+    this.socket.onerror = this.onErr;
+    this.socket.onclose = this.onClose;
+  }
+
+  before = data => data;
+
+  after = data => data;
+
+  send = (data: any) => {
+    if (!this.connecting) {
+      console.error('链接已中断');
+    } else {
+      const wrapData = this.before(data);
+      this.socket.send(wrapData);
+    }
+  }
+
+  onOpen = () => {
+    // this.params.onOpen();
+    this.connecting = true;
+    this.emit(onOpenMark, {});
+  }
+
+  onMessage = (event) => {
+    console.log(event, 'onMessage');
+    // this.params.onMessage(event);
+    const nextData = this.after(event);
+    this.emit(onMessageMark, nextData);
+  }
+
+  onErr = (e) => {
+    this.connecting = false;
+    console.log(e, 'onErr');
+  }
+
+  onClose = (e) => {
+    this.connecting = false;
+    console.log(e, 'onClose');
+  }
 }
+
+export default SocketHelper;

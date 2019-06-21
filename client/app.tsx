cd4a20bbb6becd978d4338940d30adc1f5f2ae6d
SANGET <AlexZhang@Alexs-iMac.local>
2019-06-21 18:50:54 +0800

    更新外层包装

--- client/app.tsx
+++ client/app.tsx
@@ -1,23 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import createChatStore from '@little-chat/core/store';
-import ChatApp from '@little-chat/ui/app';
+import ChatApp from '@little-chat/ui';
 import { InitSDK } from '@little-chat/sdk';
-import { Provider } from 'react-redux';
 import config from './.config';
 
 InitSDK(config);
 
-const chatStore = createChatStore({});
-
-const AppWithRedux = () => (
-  <Provider store={chatStore}>
-    <ChatApp />
-  </Provider>
-);
-
 ReactDOM.render(
-  <AppWithRedux />,
+  <ChatApp />,
   document.getElementById("Main")
 );

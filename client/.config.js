fe25f618df9d415ad01cbc4506887dd871eb058e
SANGET <3348592+SANGET@users.noreply.github.com>
2019-06-27 22:08:34 +0800

    edit config

--- client/.config.js
+++ client/.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "apiHost":process.env.NODE_ENV==='development'?"10.30.0.28:9999/gate":"52.199.38.166:9999/gate",
+  "apiHost":process.env.NODE_ENV==='development'?"ws://10.30.0.28:9999/gate":"wss://im.99safe.org/gate",
   // "apiHost":"192.168.192.145:9999/gate",
   "beforeWSHook": (data) => {
     console.log(data)

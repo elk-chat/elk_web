2d2b60b9453f711f5f2eb44c245c20d6958c7b9f
SANGET <3348592+SANGET@users.noreply.github.com>
2019-07-15 22:54:25 +0800

    添加可控的 localStorage 策略

--- packages/ui/config/app-config.ts
+++ packages/ui/config/app-config.ts
@@ -1,5 +1,17 @@
 const NavRouterMark = "N";
 
+const APP_VERSION = '3';
+const APP_VERSION_STORAGE = 'APP_VERSION_STORAGE';
+
+const appVersion = localStorage.getItem(APP_VERSION_STORAGE);
+if (appVersion !== APP_VERSION) {
+  /**
+   * 如果用户运行时的客户端版本配置与最新客户端版本要求的不一致，则清空 localStorage
+   */
+  localStorage.clear();
+}
+localStorage.setItem(APP_VERSION_STORAGE, APP_VERSION);
+
 export {
   NavRouterMark
 };

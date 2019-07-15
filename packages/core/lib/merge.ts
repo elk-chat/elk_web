d8e9092dc1610da1ad985d24b241566982b8e304
SANGET <3348592+SANGET@users.noreply.github.com>
2019-07-15 22:52:41 +0800

    merge 添加需要 toString 的格式的 key

--- packages/core/lib/merge.ts
+++ packages/core/lib/merge.ts
@@ -1,6 +1,6 @@
 import array2obj from './array2obj';
 
-export default function merge(source, target, key) {
+export default function merge(source, target, key, toString = true) {
   const sourceObj = array2obj(source, key);
   const targetObj = array2obj(target, key);
   function mergeData() {
@@ -9,6 +9,7 @@ export default function merge(source, target, key) {
       const targetItem = targetObj[ID];
       const sourceItem = sourceObj[ID];
       res[ID] = Object.assign({}, sourceItem, targetItem);
+      if (toString)res[ID][key] = res[ID][key].toString();
     });
     return res;
   }

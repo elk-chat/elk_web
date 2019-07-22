93e08816cf091e66640cb957079610283373256f
SANGET <AlexZhang@Alexs-iMac.local>
2019-07-22 16:58:52 +0800

    edit gen config

--- scripts/gen-config.js
+++ scripts/gen-config.js
@@ -6,9 +6,15 @@ const configStorePath = './client';
 
 const configPath = path.resolve(process.cwd(), configStorePath, configName)
 
+// process.argv.forEach(function (val, index, array) {
+//   console.log(index + ': ' + val);
+// });
+
 if(!fs.existsSync(configPath)) {
   const defaultConfig = {
-    apiHost: "",
+    apiHost: "wss://gate.kchat.im/gate",
+    appName: "little-chat",
+    logo: { src: '' },
     beforeWSHook: (data) => data,
     afterWSHook: (data) => data,
   }

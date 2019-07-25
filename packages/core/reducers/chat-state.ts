b0508cc48720ec84d5d681b37b2b2a6c655ee4e4
SANGET <AlexZhang@Alexs-iMac.local>
2019-07-25 16:20:05 +0800

    精简代码

--- packages/core/reducers/chat-state.ts
+++ packages/core/reducers/chat-state.ts
@@ -2,14 +2,12 @@
 import array2obj from '@little-chat/utils/array2obj';
 import getLastItem from '@little-chat/utils/get-last-item';
 import {
-  SELECT_CHAT, RECEIVE_CHAT_LIST, RECEIVE_CHAT_MESSAGE,
+  RECEIVE_CHAT_LIST, RECEIVE_CHAT_MESSAGE,
   RECEIVE_CHAT_MESSAGES,
 } from "../actions";
 import {
-  ChatActions, ChatContentState, ChatContentItem, UnreadState,
-  ChatItemEntity, ChatListEntity, ActionType, LastMsgInfo, FEMessageType
+  ChatActions, ChatListEntity, LastMsgInfo, FEMessageType
 } from '../types';
-// import { FakeChatList, getFakeChatContent } from './fake-data';
 import { getStorage, setStorage } from '../lib/storage';
 import merge from '../lib/merge';
 
@@ -41,44 +39,6 @@ export function chatListData(
 }
 
 /**
- * 记录所有 ChatContent 数据
- */
-// export function chatContentData(
-//   state: ChatContentState = {},
-//   action,
-// ) {
-//   let nextState;
-//   switch (action.type) {
-//     case SELECT_CHAT:
-//       const { ChatID = '' } = action.chatEntity;
-//       nextState = Object.assign({}, state);
-//       if (!nextState[ChatID.toString()]) {
-//         nextState[ChatID.toString()] = {
-//           data: [],
-//           lastState: 0,
-//           lastData: {},
-//         };
-//       }
-//       return nextState;
-//     case RECEIVE_CHAT_MESSAGE:
-//       nextState = Object.assign({}, state);
-//       const { chatContent, chatID } = action;
-//       if (!nextState[chatID]) nextState[chatID] = {};
-//       const currChatContent = nextState[chatID].data;
-//       const nextContent = mergeChatContent(currChatContent, chatContent);
-//       const lastData = nextContent[nextContent.length - 1] || {};
-//       nextState[chatID] = {
-//         data: nextContent,
-//         lastState: lastData.State,
-//         lastData,
-//       };
-//       return nextState;
-//     default:
-//       return state;
-//   }
-// }
-
-/**
  * 未读消息 state
  */
 export function lastMsgInfo(
@@ -91,6 +51,7 @@ export function lastMsgInfo(
       const { chatContents } = action;
       nextState = chatContents;
       setStorage('lastMsgInfo', nextState);
+      console.log(nextState)
       return nextState;
     case RECEIVE_CHAT_MESSAGE:
       const { chatContent, chatID } = action;
@@ -105,42 +66,3 @@ export function lastMsgInfo(
       return state;
   }
 }
-
-/**
- * 未读消息 state
- */
-// export function unreadInfo(
-//   state: UnreadState = {},
-//   action,
-// ) {
-//   let nextState;
-//   switch (action.type) {
-//     case RECEIVE_CHAT_MESSAGE:
-//       const { chatContent, chatID, countUnread } = action;
-//       const chatIDStr = chatID.toString();
-//       nextState = Object.assign({}, state);
-//       if (countUnread) nextState[chatIDStr] = (+nextState[chatIDStr] || 0) + chatContent.length;
-//       return nextState;
-//     case SELECT_CHAT:
-//       const { chatEntity } = action;
-//       nextState = Object.assign({}, state);
-//       nextState[chatEntity.ChatID.toString()] = 0;
-//       // nextState = Object.assign({}, state);
-//       // if (!countUnread) nextState[chatID] = (+nextState[chatID] || 0) + chatContent.length;
-//       return nextState;
-//     default:
-//       return state;
-//   }
-// }
-
-// export function selectedChat(
-//   state: ChatItemEntity = {},
-//   action,
-// ) {
-//   switch (action.type) {
-//     case SELECT_CHAT:
-//       return action.chatEntity;
-//     default:
-//       return state;
-//   }
-// }
